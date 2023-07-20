4aa2c8b7560428073b0837ef7a5ae4a57de1e8fb
marianofeld <112523084+marianofeld@users.noreply.github.com>
2023-07-19 23:54:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ let listaDeProductos = document.getElementById("productos")
 let productos = [
   { nombre: "Balanza", precio: 1000, stock: true, imagen: "./productos/balanza.webp" },
   { nombre: "Barra de sonido", precio: 3000, stock: true, imagen: "./productos/barraSonido.jpg" },
-  { nombre: "Bicicleta 1", precio: 50000, stock: true, imagen: "./productos/bici1.webp" },
-  { nombre: "Bicileta 2", precio: 100000, stock: true, imagen: "./productos/bici2.jpg" },
-  { nombre: "No se que es", precio: 10000, stock: true, imagen: "./productos/cosoRaro.webp" },
-  { nombre: "Desconosco", precio: 10000, stock: true, imagen: "./productos/desconosco.webp" },
-  { nombre: "Exprimidora", precio: 1000, stock: true, imagen: "./productos/exprimidora.webp" },
-  { nombre: "Cortadora de pelo", precio: 10000, stock: true, imagen: "./productos/maquinaPelo.webp" },
-  { nombre: "Mini", precio: 10000, stock: true, imagen: "./productos/mini.webp" },
-  { nombre: "mouseGamer", precio: 10000, stock: true, imagen: "./productos/mouseGamer.webp" },
-  { nombre: "Parlante 1", precio: 1000, stock: true, imagen: "./productos/parlante.webp" },
-  { nombre: "Parlante grande", precio: 10000, stock: true, imagen: "./productos/parlanteGrande.png" },
-  { nombre: "Plancha de pelo negra", precio: 10000, stock: true, imagen: "./productos/planchaPeloNegra.jpg" },
-  { nombre: "Parlante de pelo rosa", precio: 10000, stock: true, imagen: "./productos/planchaPeloRosa.webp" },
-  { nombre: "Plancha ropa 1", precio: 10000, stock: true, imagen: "./productos/planchaRopa.webp" },
-  { nombre: "Plancha ropa 2", precio: 10000, stock: true, imagen: "./productos/planchaRopa2.webp" },
-  { nombre: "Stereo auto", precio: 10000, stock: true, imagen: "./productos/stereo.webp" },
-  { nombre: "Trituradora", precio: 10000, stock: true, imagen: "./productos/trituradora.webp" },
-  { nombre: "Ventilador", precio: 10000, stock: true, imagen: "./productos/ventilador.webp" },
+  // { nombre: "Bicicleta 1", precio: 50000, stock: true, imagen: "./productos/bici1.webp" },
+  // { nombre: "Bicileta 2", precio: 100000, stock: true, imagen: "./productos/bici2.jpg" },
+  // { nombre: "No se que es", precio: 10000, stock: true, imagen: "./productos/cosoRaro.webp" },
+  // { nombre: "Desconosco", precio: 10000, stock: true, imagen: "./productos/desconosco.webp" },
+  // { nombre: "Exprimidora", precio: 1000, stock: true, imagen: "./productos/exprimidora.webp" },
+  // { nombre: "Cortadora de pelo", precio: 10000, stock: true, imagen: "./productos/maquinaPelo.webp" },
+  // { nombre: "Mini", precio: 10000, stock: true, imagen: "./productos/mini.webp" },
+  // { nombre: "mouseGamer", precio: 10000, stock: true, imagen: "./productos/mouseGamer.webp" },
+  // { nombre: "Parlante 1", precio: 1000, stock: true, imagen: "./productos/parlante.webp" },
+  // { nombre: "Parlante grande", precio: 10000, stock: true, imagen: "./productos/parlanteGrande.png" },
+  // { nombre: "Plancha de pelo negra", precio: 10000, stock: true, imagen: "./productos/planchaPeloNegra.jpg" },
+  // { nombre: "Parlante de pelo rosa", precio: 10000, stock: true, imagen: "./productos/planchaPeloRosa.webp" },
+  // { nombre: "Plancha ropa 1", precio: 10000, stock: true, imagen: "./productos/planchaRopa.webp" },
+  // { nombre: "Plancha ropa 2", precio: 10000, stock: true, imagen: "./productos/planchaRopa2.webp" },
+  // { nombre: "Stereo auto", precio: 10000, stock: true, imagen: "./productos/stereo.webp" },
+  // { nombre: "Trituradora", precio: 10000, stock: true, imagen: "./productos/trituradora.webp" },
+  // { nombre: "Ventilador", precio: 10000, stock: true, imagen: "./productos/ventilador.webp" },
 ]
 
 
@@ -39,27 +39,23 @@ function seVeProductos() {
   listaDeProductos.innerHTML = ""
   for (let index = 0; index < productos.length; index++) {
     if (productos[index].stock = true) {
-      listaDeProductos.innerHTML+= `${productos[index].nombre}`
+      listaDeProductos.innerHTML += `
+     
+  <div class="card">
+  <div class="card-inner">
+    <div class="card-front">
+    <p>${productos[index].nombre}</p>
+    <img src="${productos[index].imagen}" alt="">
+    </div>
+    <div class="card-back">
+      <p>Back Side</p>
+    </div>
+  </div>
+</div>
+      `
     }
     }
 }
 
 btnInicio.addEventListener("click", seVeQuienesSomos)
 btnProducto.addEventListener("click", seVeProductos)
-
-
-//  function mostrar(alimento) {
-//     colorDisplay("display", "display2", "fondoProductos", "fondoCarrito")
-//     div.innerHTML = " "
-//     for (let index = 0; index < alimento.length; index++) {
-//       div.innerHTML += `
-//     <div class="card fondoCard1 tamañoDeCartas">
-//       <img class="card-img-top" src="${alimento[index].imgUrl}" alt="Card image cap">
-//       <div class="card-body">
-//         <h5 class="card-title tamaño">${alimento[index].nombre}</h5>
-//         <p class="card-text tamaño3">Su precio es: ${alimento[index].precio * 1.25}</p>
-//         <button type="button" id="${alimento[index].id}" class="btn btn-primary boton">Añadir al pedido</button>
-//       </div>
-//     </div>
-//     `
-//     }
