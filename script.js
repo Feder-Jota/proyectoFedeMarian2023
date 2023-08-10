38b39a5a17b435f058f5e3592441a2f3dbd46aeb
marianofeld <112523084+marianofeld@users.noreply.github.com>
2023-08-10 18:45:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,34 +35,7 @@ let productos = [
   { nombre: "Ventilador", categoria: "climatizacion", precio: 10000, stock: true, imagen: "./productos/ventilador.webp" },
 ]
 
-// ver esto mas
 
-
-
-
-
-function filtrosDeProductos(par1){
-  
-    mostrar("display2", "display1")
-    listaDeProductos.innerHTML = ""
-    for (let index = 0; index < par1.length; index++) {
-      if (productos[index].stock == true) {
-        listaDeProductos.innerHTML +=
-          `
-        <div class="card fondoCard1 tamañoDeCartas">
-          <div class="divImagen">
-            <img class="card-img-top" src="${par1[index].imagen}" alt="${par1[index].nombre}">
-          </div>
-          <div class="divTexto">
-            <h5 class="card-title tamaño">${par1[index].nombre} $${par1[index].precio}</h5>
-          </div>
-          
-        </div>
-        `
-  
-      }
-    }
-  }
 let productosBelleza = productos.filter((producto) => (producto.categoria == "belleza"))
 let productosBicicleta = productos.filter((producto) => (producto.categoria == "bicicleta"))
 let productosCocina = productos.filter((producto) => (producto.categoria == "cocina"))
