53ca468fbc320a53143ceb1a4779f54dbb17d1a2
marianofeld <112523084+marianofeld@users.noreply.github.com>
2023-08-25 13:31:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -36,42 +36,43 @@ let productos = [
 ]
 
 
-let productosBelleza = productos.filter((producto) => (producto.categoria == "belleza"))
-let productosBicicleta = productos.filter((producto) => (producto.categoria == "bicicleta"))
-let productosCocina = productos.filter((producto) => (producto.categoria == "cocina"))
-let productosHogar = productos.filter((producto) => (producto.categoria == "hogar"))
-let productosTecnologia = productos.filter((producto) => (producto.categoria == "tecnologia"))
-let productosClimatizacion = productos.filter((producto) => (producto.categoria == "climatizacion"))
+let productosBelleza = productos.filter((productos) => (productos.categoria == "belleza"))
+let productosBicicleta = productos.filter((productos) => (productos.categoria == "bicicleta"))
+let productosCocina = productos.filter((productos) => (productos.categoria == "cocina"))
+let productosHogar = productos.filter((productos) => (productos.categoria == "hogar"))
+let productosTecnologia = productos.filter((productos) => (productos.categoria == "tecnologia"))
+let productosClimatizacion = productos.filter((productos) => (productos.categoria == "climatizacion"))
 
-
-
-
+document.addEventListener('touchstart',{passive: true})
 
 
 
 
 function mostrarBotonera(display1, display2) {
-botoneraFiltros.classList.add(`${display1}`)
+ 
+ botoneraFiltros.classList.add(`${display1}`)
  botoneraFiltros.classList.remove(`${display2}`)  
  }
 
-
-
 function mostrar(display1, display2) {
+  
   quienesSomos.classList.add(`${display1}`)
   quienesSomos.classList.remove(`${display2}`)
   listaDeProductos.classList.add(`${display2}`)
   listaDeProductos.classList.remove(`${display1}`)
-
 }
 
 function seVeQuienesSomos() {
+  
   listaDeProductos.innerHTML = ""
   mostrar("display1", "display2")
   mostrarBotonera("display2","display1")
 }
 
-function seVeProductos() {
+
+
+function filtrosDeProductos(producto) {
+  
   mostrarBotonera("display1","display2")
   mostrar("display2", "display1")
   listaDeProductos.innerHTML = ""
@@ -81,28 +82,26 @@ function seVeProductos() {
         `
       <div class="card fondoCard1 tamañoDeCartas">
         <div class="divImagen">
-          <img class="card-img-top" src="${productos[index].imagen}" alt="${productos[index].nombre}">
+          <img class="card-img-top" src="${producto[index].imagen}" alt="${producto[index].nombre}">
         </div>
         <div class="divTexto">
-          <h5 class="card-title tamaño">${productos[index].nombre} $${productos[index].precio}</h5>
+          <h5 class="card-title tamaño">${producto[index].nombre} $${producto[index].precio}</h5>
         </div>
         
       </div>
       `
-
     }
   }
 }
 
 
 btnInicio.addEventListener("click", seVeQuienesSomos)
-btnProducto.addEventListener("click", seVeProductos)
+btnProducto.onclick = () => { filtrosDeProductos(productos)}
 belleza.onclick = () => { filtrosDeProductos(productosBelleza) }
 bicicleta.onclick = () => { filtrosDeProductos(productosBicicleta) }
 hogar.onclick = () => { filtrosDeProductos(productosHogar) }
 cocina.onclick = () => { filtrosDeProductos(productosCocina) }
 tecnologia.onclick = () => { filtrosDeProductos(productosTecnologia) }
 climatizacion.onclick = () => { filtrosDeProductos(productosClimatizacion) }
-
-todo.addEventListener("click", seVeProductos)
+todo.onclick = () => { filtrosDeProductos(productos) }
 
